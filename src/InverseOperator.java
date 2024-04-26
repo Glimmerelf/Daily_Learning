f447f0d71b94d67a19e08e06455875b86aab4d95
glimmerelf <2020239193@.qq.com>
2024-04-27 00:02:23 +0800

    !取反^逻辑异或

---
+++ src/InverseOperator.java
@@ -0,0 +1,12 @@
+/**
+ * @author Glimmer
+ * @version v1.0
+ * @ClassName InverseOperator
+ * @Description !取反^逻辑异或
+ * @CreateTime 2024/4/26 18:38
+ */
+public class InverseOperator {
+    public static void main(String[] args) {
+
+    }
+}
