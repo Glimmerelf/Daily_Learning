46437c8bed311e720c35260a8ea0c406ebd70d00
Glimmerelf <2020239193@qq.com>
2024-04-27 15:48:09 +0800

    !取反^逻辑异或

--- src/InverseOperator.java
+++ src/InverseOperator.java
@@ -7,6 +7,13 @@
  */
 public class InverseOperator {
     public static void main(String[] args) {
-
+//        !操作是取反 True 变为 False，False 变为 True
+        System.out.println(60 > 20);// 输出 True
+        System.out.println(!(60 > 20));// 输出 False
+//        a^b 逻辑异或，当a和b不同时，结果为True，否则为False
+        boolean a = (10 > 1) ^ (3 > 5);
+        System.out.println("a = " + a);// 输出 True
+        boolean b = (10 > 1) ^ (3 < 5);
+        System.out.println("b = " + b);// 输出 False
     }
 }
